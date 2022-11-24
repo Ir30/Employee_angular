2eee0ce4f3ae1ee565568e12bf1fd94fab564e9d
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-24 16:57:23 +0530

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,60 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { SearchComponent } from './search/search.component';
+import { EditComponent } from './edit/edit.component';
+import { DeleteComponent } from './delete/delete.component';
+import { ViewAllEmpComponent } from './view-all-emp/view-all-emp.component';
+import { NavComponent } from './nav/nav.component';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdminLoginComponent
+  },
+  {
+    path:"add",
+    component:AddComponent
+  },
+  {
+    path:"search",
+    component:SearchComponent
+  },
+  {
+    path:"edit",
+    component:EditComponent
+  },
+  {
+    path:"delete",
+    component:DeleteComponent
+  },
+  {
+    path:"view",
+    component:ViewAllEmpComponent
+  }
+
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    AddComponent,
+    SearchComponent,
+    EditComponent,
+    DeleteComponent,
+    ViewAllEmpComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
